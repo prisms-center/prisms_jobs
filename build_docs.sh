a0f32ef1a0464a3facdcda43f7d7d39f91000470
bpuchala <bpuchala@users.noreply.github.com>
2017-09-12 15:53:52 -0400

    fix 'build_docs.sh' script

--- build_docs.sh
+++ build_docs.sh
@@ -2,4 +2,4 @@ python setup.py build_sphinx
 rm -r ../prisms_jobs_docs/*
 cp -r doc/build/html/* ../prisms_jobs_docs
 cp doc/source/README.md ../prisms_jobs_docs/
-cd ../prisms_jobs_docs && git add -u && git push
+cd ../prisms_jobs_docs && git add -u && git commit -m "rebuild" && git push
